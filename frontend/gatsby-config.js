6c1d46678b5557f7c1439faaa32f72809c2cf888
Tomas Hamsa <multipristr@gmail.com>
2021-06-16 15:26:21 +0200

    Frontend skeleton

---
+++ frontend/gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    siteMetadata: {
+        title: "Message board",
+        author: "Tomas Hamsa",
+        siteUrl: "http://localhost:80/",
+    },
+    plugins: []
+}
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+const IndexPage = () => {
+    return (
+        <main>
+            <title>Message board</title>
+            message
+        </main>
+    )
+}
+
+export default IndexPage
