8cc9706063bf464f32faf0203a5b45b705a8626b
Tomas Hamsa <multipristr@gmail.com>
2021-06-18 18:07:38 +0200

    Add api calls skeleton

--- frontend/src/components/AddMessage.js
+++ frontend/src/components/AddMessage.js
@@ -1,17 +1,38 @@
 import * as React from "react"
+import {SERVER_URL} from "../config";
 
 const areaStyle = {
-    display: "grid",
     backgroundColor: "black",
     color: "white",
     margin: "0.2%",
 }
 
+const createMessage = (parentId, content) => {
+    let url = `${SERVER_URL}/message`
+    if (parentId) {
+        url += `?parentId=${parentId}`
+    }
+    return fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            contentType: "application/json",
+        },
+        body: JSON.stringify(content),
+    })
+}
+
+
 const AddMessage = ({parentId}) => {
     return (
         <div style={areaStyle}>
             <textarea/>
-            <button>Post</button>
+            <button onClick={() =>
+                createMessage(parentId, "content")
+                    .then(() => "") // TODO FIXME Reload messages
+            }>
+                Post
+            </button>
         </div>
     )
 }
--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,4 +1,5 @@
 import * as React from "react"
+import {SERVER_URL} from "../config";
 
 const messageStyle = {
     backgroundColor: "black",
@@ -11,16 +12,41 @@ const headStyle = {
     fontFamily: "monospace",
 }
 
+const deleteMessage = (id) => fetch(`${SERVER_URL}/message/${id}`, {
+    method: 'DELETE',
+    credentials: 'include',
+})
+
+const modifyMessage = (id, content) => fetch(`${SERVER_URL}/message${id}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+        contentType: "application/json",
+    },
+    body: JSON.stringify(content),
+})
+
 const Message = ({id, author, createdAt, lastModifiedAt, content, addReplies}) => {
     return (
-        <div style={messageStyle} id={id}>
+        <div style={messageStyle}>
             <div>
-                <text style={headStyle}>
+                <div style={headStyle}>
                     {author}&nbsp;Created: <time>{new Date(createdAt).toLocaleString()}</time>&nbsp;
                     {lastModifiedAt !== createdAt && <>Modified: <time>{new Date(lastModifiedAt).toLocaleString()}</time>&nbsp;</>}
-                </text>
-                <button>Modify</button>
-                <button>Delete</button>
+                </div>
+                <button onClick={() => {
+                    // TODO FIXME Make content editable
+                    // TODO FIXME Select new content
+                    modifyMessage(id, "new content")
+                        .then(() => "")// TODO FIXME Reload message list
+                }}>
+                    Modify
+                </button>
+                <button onClick={() => deleteMessage(id)
+                    .then(() => "") // TODO FIXME Reload message list
+                }>
+                    Delete
+                </button>
             </div>
             <p>{content}</p>
             <div>
