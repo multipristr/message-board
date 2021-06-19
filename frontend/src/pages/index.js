faf1e6c3a46fc4340fc7e44863308364d609844c
Tomas Hamsa <multipristr@gmail.com>
2021-06-19 14:10:04 +0200

    Separate register form

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,14 +1,11 @@
 import * as React from "react"
 import MessageList from "../components/MessageList";
-import Login from "../components/Login";
 
-
-const IndexPage =  () => {
-    const isLoggedIn = false // TODO FIXME verify login
+const IndexPage = () => {
     return (
         <main>
             <title>Message board</title>
-            {isLoggedIn ? <MessageList level={-1}/> : <Login/>}
+            <MessageList level={-1}/>
         </main>
     )
 }
